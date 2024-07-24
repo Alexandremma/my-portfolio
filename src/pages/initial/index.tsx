852e705d1dbd966e4f239925fc845371924e1cab
ammac <alexandremmac@gmail.com>
2024-07-24 18:43:11 -0300

    teste

--- src/pages/initial/index.tsx
+++ src/pages/initial/index.tsx
@@ -63,7 +63,7 @@ export default function InitialPage() {
                         </button>
                     </div>
                     <div>
-
+                        oi
                     </div>
                 </section>
 
